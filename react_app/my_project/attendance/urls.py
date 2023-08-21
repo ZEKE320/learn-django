1a90f766f3c47b6725ad2e6f182821bd8f9124b6
ZEKE320 <46393375+ZEKE320@users.noreply.github.com>
2023-08-21 20:23:14 +0900

    Revert "Django REST Frameworkによる実装を追加"

--- react_app/my_project/attendance/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.views.generic import TemplateView
-
-from . import views
-
-app_name = 'attendance'
-urlpatterns = [
-    path('', views.AttendanceListAPIView.as_view(), name='attendance'),
-]
--- react_app/next/attendance/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function hoge(arg) {
-    return String(arg);
-}
-console.log(hoge(100));
